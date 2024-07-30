3e6cf19394e6059031e2cbd0a6f29fafdba5640b
JinKyu <rlawlsrb167@naver.com>
2024-07-31 04:03:36 +0900

    webpack.config.js plugins + devServer 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "bundle.[hash].js",
   },
   module: {
     rules: [
@@ -16,6 +17,21 @@ module.exports = {
         },
       },
     ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
   },
   mode: "development",
 };
